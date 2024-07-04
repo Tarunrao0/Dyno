6c9fd9dbc7bdc480f5d8494874af55aee5323c6b
richard jophy <richard.jophy@gmail.com>
2024-07-05 02:27:55 +0530

    connect page

--- client/src/components/ConnectWallet.jsx
+++ client/src/components/ConnectWallet.jsx
@@ -1,11 +1,13 @@
 import { ethers } from "ethers";
 import { useState } from "react";
 import classes from "./ConnectWallet.module.css";
+import dynoImage from "../assets/dyno.png"; // Import the image correctly
 
 export default function ConnectWallet() {
   const [address, setAddress] = useState("");
   const [signer, setSigner] = useState("");
   const [connected, setConnection] = useState(false);
+  const [hovered, setHovered] = useState(false); // State to track hover state
 
   const connectWallet = async () => {
     if (!window.ethereum) {
@@ -37,18 +39,24 @@ export default function ConnectWallet() {
 
   return (
     <div className={classes.container}>
+       
       <div className={classes.navbar}>
-        <div className={classes.logo}>DYNO</div>
+      <div className={classes.logo}> <img src={dynoImage} alt="dyno" />
+      </div>
         <a href="#home">Buy Energy</a>
         <a href="#buy">Buy/Sell</a>
         <a href="#about">About Us</a>
       </div>
       <div className={classes["btn-container"]}>
-        <button className={classes["btn-36"]} onClick={connectWallet}>
-          {connected ? shortenAddress(address) : "Connect"}
+        <button
+          className={classes["btn-36"]}
+          onClick={connectWallet}
+          onMouseEnter={() => setHovered(true)} // Set hovered state on mouse enter
+          onMouseLeave={() => setHovered(false)} // Reset hovered state on mouse leave
+        >
+          {connected ? shortenAddress(address) : hovered ? "Wallet" : "Connect"}
         </button>
       </div>
     </div>
   );
 }
-
