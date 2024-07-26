00335cbb7285bb165f77d179c429701b21265134
aizen <tarunsanjayrao@gmail.com>
2024-07-26 21:34:59 +0530

    marketplace-100

--- client/src/app/marketplace/page.tsx
+++ client/src/app/marketplace/page.tsx
@@ -11,6 +11,8 @@ import {
   dynoTokenAbi,
 } from "../../../constants/constants";
 import { useSellDyno } from "@/hooks/use-sell-dyno";
+import Image from "next/image";
+import marketplace from "../../../public/marketplace.png";
 
 export default function BuySell() {
   const { address: ownerAddress } = useAccount();
@@ -65,55 +67,48 @@ export default function BuySell() {
       <div className="mt-24 ml-36 text-6xl font-black text-inkblue">
         <h1>Power Up Your Portfolio with Dyno Energy Credits!</h1>
       </div>
-      <div>
-        <div className="mt-20 ml-40 text-xl w-2/5">
-          <h3 className="font-merchandise text-3xl text-inkblue">
-            Getting started :{" "}
-          </h3>
-          <div className=" font-semibold text-xl">
+      <div className="mt-20 ml-40 text-xl">
+        <h3 className="font-merchandise text-3xl text-inkblue">TOKENOMICS: </h3>
+        <div className="flex items-start w-full">
+          <div className="font-semibold text-xl w-2/5">
             <ul className="mt-2">
-              <p>
+              <li>
                 Dyno Energy Credits are ERC-20 tokens that represent your share
                 of sustainable energy.
-              </p>
-              <p>Each Dyno is valued 1:1 with USDC, ensuring stability.</p>
-              <p> One Dyno token equals 100 kWh of energy.</p>
-              <p>Dyno tokens can be seamlessly swapped back to USDC.</p>
+              </li>
+              <li>Each Dyno is valued 1:1 with USDC, ensuring stability.</li>
+              <li>One Dyno token equals 100 kWh of energy.</li>
+              <li>Dyno tokens can be seamlessly swapped back to USDC.</li>
             </ul>
+          </div>
+          <div className="ml-72 -mt-20">
+            <Image src={marketplace} alt="asterisk" width={250} />
           </div>
         </div>
       </div>
-      <div className="w-80 p-3 ml-40 mt-16">
-        <form onSubmit={handleBuy}>
-          <label className="font-black text-inkblue">
-            Enter Amount to buy:{" "}
-          </label>
-          <div className="flex">
-            <input
-              className="bg-almond/50 w-80 h-7 rounded-2xl mr-4 text-center font-black"
-              name="amount"
-              placeholder="DYNO"
-            />
-            <button
-              className=" bg-inkblue font-black px-8 text-oatmilk rounded-2xl"
-              disabled={isPending}
-              type="submit"
-            >
-              {isPending ? "Confirming..." : "Buy"}
-            </button>
-          </div>
-        </form>
-        <div>
-          {hash && <div>Transaction Hash: {hash}</div>}
-          {isConfirming && <div>Waiting for confirmation...</div>}
-          {isConfirmed && <div>Transaction confirmed.</div>}
-          {error && (
-            <div>
-              Error: {(error as BaseError).shortMessage || error.message}
+      <div className="w-80 p-3 ml-40 mt-16 flex relative">
+        <div className="mt-3">
+          <form onSubmit={handleBuy}>
+            <label className="font-black text-inkblue">
+              Enter Amount to buy:{" "}
+            </label>
+            <div className="flex">
+              <input
+                className="bg-almond/50 w-80 h-7 rounded-2xl mr-4 text-center font-black"
+                name="amount"
+                placeholder="DYNO"
+              />
+              <button
+                className=" bg-inkblue font-black px-8 text-oatmilk rounded-2xl transition-transform duration-300 hover:-translate-y-2"
+                disabled={isPending}
+                type="submit"
+              >
+                {isPending ? "Confirming..." : "Buy"}
+              </button>
             </div>
-          )}
+          </form>
         </div>
-        <div className="w-80 p-3 ml-40 mt-16">
+        <div className="w-80 p-3 ml-44">
           <form onSubmit={handleSell}>
             <label className="font-black text-inkblue">
               Enter Amount to sell:{" "}
@@ -125,7 +120,7 @@ export default function BuySell() {
                 placeholder="USDC"
               />
               <button
-                className=" bg-inkblue font-black px-8 text-oatmilk rounded-2xl"
+                className=" bg-inkblue font-black px-8 text-oatmilk rounded-2xl transition-transform duration-300 hover:-translate-y-2"
                 disabled={isPending}
                 type="submit"
               >
@@ -133,10 +128,28 @@ export default function BuySell() {
               </button>
             </div>
           </form>
+        </div>
+        <div className=" flex absolute left-0 top-0 mt-24 ml-3 font-black">
           <div>
-            {selltxhash && <div>Transaction Hash: {hash}</div>}
-            {selltxConfirming && <div>Waiting for confirmation...</div>}
-            {selltxConfirmed && <div>Transaction confirmed.</div>}
+            {hash && <div>Transaction Hash: {hash}</div>}
+            {isConfirming && <div>Waiting for confirmation...</div>}
+            {isConfirmed && <div>Transaction confirmed 🎉.</div>}
+            {error && (
+              <div>
+                Error: {(error as BaseError).shortMessage || error.message}
+              </div>
+            )}
+          </div>
+          <div className="ml-96">
+            {selltxhash && (
+              <div className="ml-24">Transaction Hash: {hash}</div>
+            )}
+            {selltxConfirming && (
+              <div className="ml-24">Waiting for confirmation...</div>
+            )}
+            {selltxConfirmed && (
+              <div className="ml-24">Transaction confirmed 🎉.</div>
+            )}
             {selltxerror && (
               <div>
                 Error:{" "}
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -19,18 +19,18 @@ export default function NavBar() {
   return (
     <div>
       <nav className="flex items-center justify-between">
-        <div className="text-inkblue ml-20 text-5xl font-superWoobly mt-9">
+        <div className="text-inkblue ml-20 text-5xl font-superWoobly mt-9 transition-transform duration-300 hover:-translate-y-2">
           <Link href="/">DYNO</Link>
         </div>
         <div className="flex items-center space-x-4 mr-20 mt-8">
           <Link href="marketplace" className="px-20">
-            <p className="text-inkblue w-full text-xl font-black mt-3 mr-20">
+            <p className="text-inkblue w-full text-xl font-black mt-3 mr-20 transition-transform duration-300 hover:-translate-y-2">
               Marketplace
             </p>
           </Link>
           <button
             onClick={handleConnect}
-            className="bg-inkblue text-oatmilk px-3 py-2 rounded-xl font-black"
+            className="bg-inkblue text-oatmilk px-3 py-2 rounded-xl font-black transition-transform duration-300 hover:-translate-y-2"
           >
             {connected ? shortenAddress(address) : "Connect"}
           </button>
